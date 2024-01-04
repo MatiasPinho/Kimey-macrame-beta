7140120206d5d573990b4a442cacfeeab91d9154
Matias Pinho <mati1234661mto@gmail.com>
2024-01-04 10:25:47 -0300

    navbar arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import { Footer } from "./components/Footer/Footer";
 function App() {
   return (
     <>
+      <Header></Header>
       <main>
-        <Header></Header>
         <HeroSection></HeroSection>
         <AboutSection></AboutSection>
         <PersonalizationSection></PersonalizationSection>
